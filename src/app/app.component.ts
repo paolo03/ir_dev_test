2630a5438962d0f95a58fa97591bcd969be8400a
IR <na@intelligentreach.com>
2022-07-13 02:51:03 +0800

    Implement brand and stock filter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { FormBuilder, FormGroup } from "@angular/forms";
 import { Observable } from "rxjs";
-import { map } from "rxjs/operators";
+import { map, tap } from "rxjs/operators";
 import { Product } from "./shared/models/product.model";
 import { ProductsService } from "./shared/services/products.service";
 
@@ -16,6 +16,8 @@ export class AppComponent implements OnInit{
   product$: Observable<Product[]>
   filteredProducts$: Observable<Product[]>
   searchForm: FormGroup
+  stockFilter: String = "all"
+  brandFilter: String
 
   constructor(
     private productService: ProductsService,
@@ -30,11 +32,38 @@ export class AppComponent implements OnInit{
 
   ngOnInit(): void {
     this.productBrands$ = this.product$
-    .pipe(map(products => Array.from(new Set(products.map(
-      (product) => product.brand)))
+      .pipe(map(products => Array.from(new Set(products.map(
+        (product) => product.brand)))
+      ),tap((data)=> {
+        this.brandFilter = data[0]
+        this.filterProducts()
+      }
     ))
   }
 
+  filterStocks(quantity: number): boolean{
+    if(this.stockFilter == 'in-stock'){
+        return quantity > 0
+    } else if(this.stockFilter == 'out-of-stock') {
+        return quantity <= 0
+    } else {
+      return true
+    }
+  }
 
- 
+  filterProducts(){
+    this.filteredProducts$ = this.product$.pipe(
+      map(product => product.filter(product => product.brand === this.brandFilter &&  this.filterStocks(product.quantity) ))
+    )
+  }
+
+  onChangeBrand(e: any) {
+    this.brandFilter = e.target.value
+    this.filterProducts()
+  } 
+
+  onChangeRadio(e: any) {
+    this.stockFilter = e.target.value;
+    this.filterProducts()
+  } 
 }
