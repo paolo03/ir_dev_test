7335c118808f2b7f10faafdac4adc2759a319132
IR <na@intelligentreach.com>
2022-07-12 23:42:49 +0800

    Populate brand select options

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { Observable } from "rxjs";
+import { filter, map } from "rxjs/operators";
 import { Product } from "./shared/models/product.model";
 import { ProductsService } from "./shared/services/products.service";
 
@@ -8,11 +9,23 @@ import { ProductsService } from "./shared/services/products.service";
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.css"]
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = "ir-dev-test";
+  productBrands$: Observable<String[]>
   product$: Observable<Product[]>
 
   constructor(private productService: ProductsService) {
     this.product$ = this.productService.getProducts() as Observable<Product[]>
   }
+
+  ngOnInit(): void {
+    this.productBrands$ = this.product$
+    .pipe(map(products => Array.from(new Set(products.map(
+      (product) => product.brand)))
+    ))
+
+    
+  }
+
+ 
 }
